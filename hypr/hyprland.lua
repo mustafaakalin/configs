f6c807f1269ba11f355af7affb896064d7e5b8e4
MustafaBABA <mustafa0xall@gmail.com>
2026-08-01 23:31:17 +0300

    add:udiskie:hyprland.lua

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -48,7 +48,7 @@ local menu = "hyprlauncher"
 hl.on("hyprland.start", function()
 	hl.exec_cmd(terminal)
 	hl.exec_cmd("nm-applet")
-	hl.exec_cmd("waybar & hyprpaper & swaync & firefox")
+	hl.exec_cmd("udiskie & waybar & hyprpaper & swaync & firefox")
 end)
 
 -------------------------------
