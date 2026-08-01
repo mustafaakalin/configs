be7963362a01b228c2c2d4952b883c7ac5a040a9
MustafaBABA <mustafa0xall@gmail.com>
2026-08-01 20:17:53 +0300

    nvim updated

---
+++ nvim/lua/plugins/colorschemes.lua
@@ -0,0 +1,10 @@
+return {
+
+  -- coloschemess
+  { "rafi/awesome-vim-colorschemes" },
+  { "mcchrish/vim-no-color-collections" },
+  { "rainglow/vim" },
+  { "chriskempson/base16-vim" },
+  { "mkarmona/colorsbox" },
+  { "EdenEast/nightfox.nvim" },
+}
